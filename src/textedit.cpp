ca0bc7a781656d43f59ff32e77356c16753d0d1a
Bertrand Songis <bsongis@gmail.com>
2021-07-05 19:42:58 +0200

    Wrong define used

--- src/textedit.cpp
+++ src/textedit.cpp
@@ -89,7 +89,7 @@ void TextEdit::onEvent(event_t event)
 {
   TRACE_WINDOWS("%s received event 0x%X", getWindowDebugString().c_str(), event);
 
-#if defined(VIRTUAL_KEYBOARD)
+#if defined(SOFTWARE_KEYBOARD)
   if (IS_VIRTUAL_KEY_EVENT(event)) {
     uint8_t c = event & 0xFF;
     if (c == (uint8_t)KEYBOARD_BACKSPACE[0]) {
