836c92d0219c45dc0da4a10b172b8d00105d75f5
Bertrand Songis <bsongis@gmail.com>
2021-12-15 18:06:49 +0100

    Fixes

--- src/bitmapbuffer.cpp
+++ src/bitmapbuffer.cpp
@@ -326,9 +326,9 @@ bool BitmapBuffer::clipLine(coord_t & x1, coord_t & y1, coord_t & x2, coord_t & 
   float p4 = -p3;
 
   float q1 = x1 - xmin;
-  float q2 = xmax - x1;
+  float q2 = xmax - 1 - x1;
   float q3 = y1 - ymin;
-  float q4 = ymax - y1;
+  float q4 = ymax - 1 - y1;
 
   float posarr[5], negarr[5];
   int posind = 1, negind = 1;
@@ -394,8 +394,12 @@ void BitmapBuffer::drawLine(coord_t x1, coord_t y1, coord_t x2, coord_t y2, LcdC
   x2 += offsetX;
   y2 += offsetY;
 
+  TRACE("");
+  TRACE("AVANT %d %d %d %d", x1, y1, x2, y2);
   if (!clipLine(x1, y1, x2, y2))
     return;
+  TRACE("CLIPPING %d-%d %d-%d", xmin, xmax, ymin, ymax);
+  TRACE("APRES %d %d %d %d", x1, y1, x2, y2);
 
   auto rgb565 = COLOR_TO_RGB565(color);
 
