10ad6b087857b4cfb5b1a67fbf93b1cbf724b200
Nicholas-Luoyi <18351579360@163.com>
2026-05-06 15:13:28 +0800

    Fix for drawPlainFilledRectangle

--- src/bitmapbuffer.cpp
+++ src/bitmapbuffer.cpp
@@ -885,10 +885,7 @@ void BitmapBuffer::fillRectangle(coord_t x, coord_t y, coord_t w, coord_t h, pix
 
 void BitmapBuffer::drawPlainFilledRectangle(coord_t x, coord_t y, coord_t w, coord_t h, Color565 color)
 {
-  if (format == BMP_RGB565)
-    fillRectangle(x, y, w, h, color);
-  else
-    fillRectangle(x, y, w, h, RGB565_TO_ARGB4444(color, 0xFF));
+  fillRectangle(x, y, w, h, color);
 }
 
 void BitmapBuffer::drawMaskFilledRectangle(coord_t x, coord_t y, coord_t w, coord_t h, const BitmapMask * mask, Color565 color)
