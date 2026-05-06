fb7bf071ba6d8ecaad4bb519ec2f5341d298630a
Nicholas-Luoyi <18351579360@163.com>
2026-05-06 14:01:45 +0800

    Adapt to ARGB4444 for DMAFillRect

--- src/bitmapbuffer.cpp
+++ src/bitmapbuffer.cpp
@@ -877,7 +877,7 @@ void BitmapBuffer::fillRectangle(coord_t x, coord_t y, coord_t w, coord_t h, pix
   if (!applyClippingRect(x, y, w, h))
     return;
 
-  DMAFillRect(data, _width, _height, x, y, w, h, pixel);
+  DMAFillRect(data, _width, _height, x, y, w, h, format == BMP_ARGB4444, format == BMP_ARGB4444 ? RGB565_TO_ARGB4444(pixel, 0xFF) : pixel);
 }
 
 void BitmapBuffer::drawPlainFilledRectangle(coord_t x, coord_t y, coord_t w, coord_t h, Color565 color)
