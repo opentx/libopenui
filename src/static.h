88b6061b8fc9c14fbdb3738ba59c811d61a3b310
bsongis <bsongis@gmail.com>
2026-06-02 20:25:45 +0200

    StaticText::setTextColor() should invalidate the window

--- src/static.h
+++ src/static.h
@@ -77,6 +77,7 @@ class StaticText: public Window
     void setTextColor(LcdColor value)
     {
       textColor = value;
+      invalidate();
     }
 
     void setPadding(coord_t horizontalPadding, coord_t verticalPadding)
